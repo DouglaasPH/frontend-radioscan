83b99ade61f81499eef47fd5b1c8f2880f3fce16
Douglas Phelipe <douglasphelipedpss@gmail.com>
2026-07-15 20:56:00 -0300

    feat: add create account page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,19 @@
 import { Routes } from '@angular/router';
-import { Login } from './components/login/login';
+import { Login } from './pages/login/login';
+import { CreateAccount } from './pages/create-account/create-account';
 
 export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
   {
     path: 'login',
     component: Login,
   },
   {
-    path: '',
-    redirectTo: 'login',
-    pathMatch: 'full',
+    path: 'create-account',
+    component: CreateAccount,
   },
 ];

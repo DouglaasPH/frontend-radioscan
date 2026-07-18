12cab18c0c3a22c963ced6c862cb58512e1e4358
Douglas Phelipe <douglasphelipedpss@gmail.com>
2026-07-18 03:15:09 -0300

    feat: add loading state component

---
+++ src/app/components/loading-state/loading-state.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'loading-state-component',
+  imports: [],
+  templateUrl: './loading-state.component.html',
+  styleUrl: './loading-state.component.css',
+})
+export class LoadingStateComponent {}
